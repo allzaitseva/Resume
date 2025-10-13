7cbe1cbb4dacf69eae30194f8ec1f0cb9b45497c
Alena Zaitseva <alena@MBP-George.telekom.ip>
2025-10-13 17:27:57 +0200

    ...

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import Hero from './components/Hero'
+
+export default function App() {
+  return (
+    <div className='bg-slate-800'>
+      <Hero />
+    </div>
+  )
+}
---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Hero() {
+    return (
+        <section className="h-100 w-full bg-slate-700">
+
+        </section>
+    )
+}
